635bff0457a3c77b2e85c4d42a2a8bfbf6270deb
David <davidanthonycolucci@gmail.com>
2025-06-13 10:42:50 -0400

    add basic content structure, nav bar, stylesheet, and modify config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
-module.exports = {
-    dir: {
-        input: "src",
-        output: "_site"
-    }
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.setInputDirectory("src");
+    eleventyConfig.setOutputDirectory("_site");
 };
