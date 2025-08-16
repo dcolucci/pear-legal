3a194c1ad5d31ccd554534024fb4d10e5babd4d8
David <davidanthonycolucci@gmail.com>
2025-08-16 15:19:39 -0400

    adjusting messages on contact form

--- src/assets/js/formspree-submit.js
+++ src/assets/js/formspree-submit.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }).then(response => {
             if (response.ok) {
-                status.innerHTML = "Thanks for your submission!";
+                status.innerHTML = "Thank you for your message! We will get in touch soon.";
                 form.reset();
                 form.classList.add('form-submitted');
             } else {
@@ -23,12 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     if (Object.hasOwn(data, 'errors')) {
                         status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
                     } else {
-                        status.innerHTML = "Oops! There was a problem submitting your form";
+                        status.innerHTML = "Sorry, there was a problem. Please try again later.";
                     }
                 });
             }
         }).catch(error => {
-            status.innerHTML = "Oops! There was a problem submitting your form"
+            status.innerHTML = "Sorry, there was a problem. Please try again later."
         });
     }
 
